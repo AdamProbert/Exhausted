3800e131d2b31fab98fc05bada882c7f712a812a
Adam Probert <adamprobert@live.co.uk>
2020-02-05 18:46:30 +0000

    Added braking to car

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -6,13 +6,19 @@ public class CarController : MonoBehaviour
 {
     private float horizontalInput;
     private float verticalInput;
-
     private float steeringAngle;
+    private Vector3 velocity;
+
+    [SerializeField][Range(0,1)][Tooltip("Decimal multiplied by usual wheel collider friction attributes. Lower number = higher skid")]
+     private float brakeSkidMultiplier;
+
+    private bool braking;
     
     [SerializeField] private WheelCollider f_r_wheel, f_l_wheel, r_r_wheel, r_l_wheel;
     [SerializeField] private Transform f_r_transform, f_l_transform, r_r_transform, r_l_transform;
     [SerializeField] private float maxSteerAngle;
     [SerializeField] private float motorForce;
+    [SerializeField] private float brakeTorque;
     [SerializeField] private Rigidbody carRigidBody;
     [SerializeField] private Vector3 carCenterOfMass;
     [SerializeField] private bool frontWheelDrive;
@@ -34,17 +40,94 @@ public class CarController : MonoBehaviour
 
     private void Accelerate()
     {
-        if(frontWheelDrive)
+
+        velocity = transform.InverseTransformDirection(carRigidBody.velocity);
+        
+        if (velocity.z > 0 && verticalInput < 0)
         {
-            f_r_wheel.motorTorque = verticalInput * motorForce;
-            f_l_wheel.motorTorque = verticalInput * motorForce;
+            if(!braking)
+            {
+                // We're braking
+                BrakeAllWheels(true);
+            }
         }
         else
         {
-            r_r_wheel.motorTorque = verticalInput * motorForce;
-            r_l_wheel.motorTorque = verticalInput * motorForce;
+            BrakeAllWheels(false);
+            // We're moving
+            if(frontWheelDrive)
+            {
+                f_r_wheel.motorTorque = verticalInput * motorForce;
+                f_l_wheel.motorTorque = verticalInput * motorForce;
+            }
+            else
+            {
+                r_r_wheel.motorTorque = verticalInput * motorForce;
+                r_l_wheel.motorTorque = verticalInput * motorForce;
+            }    
+        }        
+    }
+
+    private void BrakeAllWheels(bool brake)
+    {
+        if(brake & !braking)
+        {
+            Debug.Log("Braking");
+            Brake(f_r_wheel);
+            Brake(f_l_wheel);
+            Brake(r_r_wheel);
+            Brake(r_l_wheel);
+            braking = true;
         }
-        
+
+        if(!brake & braking)
+        {
+            Debug.Log("Releaing brake");
+            ReleaseBrake(f_r_wheel);
+            ReleaseBrake(f_l_wheel);
+            ReleaseBrake(r_r_wheel);
+            ReleaseBrake(r_l_wheel);
+            braking = false;
+        }
+    }
+
+    private void Brake(WheelCollider wheel)
+    {
+        wheel.brakeTorque = brakeTorque;
+
+        WheelFrictionCurve forwardFriction = wheel.forwardFriction;
+        forwardFriction.extremumSlip *= brakeSkidMultiplier;
+        forwardFriction.extremumValue *= brakeSkidMultiplier;
+        forwardFriction.asymptoteSlip *= brakeSkidMultiplier;
+        forwardFriction.asymptoteValue *= brakeSkidMultiplier;
+
+        wheel.forwardFriction = forwardFriction;
+
+        WheelFrictionCurve sidewaysFriction = wheel.sidewaysFriction;
+        sidewaysFriction.extremumSlip *= brakeSkidMultiplier;
+        sidewaysFriction.extremumValue *= brakeSkidMultiplier;
+        sidewaysFriction.asymptoteSlip *= brakeSkidMultiplier;
+        sidewaysFriction.asymptoteValue *= brakeSkidMultiplier;
+        wheel.sidewaysFriction = sidewaysFriction;
+    }
+
+    private void ReleaseBrake(WheelCollider wheel)
+    {
+        wheel.brakeTorque = 0;
+
+        WheelFrictionCurve forwardFriction = wheel.forwardFriction;
+        forwardFriction.extremumSlip /= brakeSkidMultiplier;
+        forwardFriction.extremumValue /= brakeSkidMultiplier;
+        forwardFriction.asymptoteSlip /= brakeSkidMultiplier;
+        forwardFriction.asymptoteValue /= brakeSkidMultiplier;
+        wheel.forwardFriction = forwardFriction;
+
+        WheelFrictionCurve sidewaysFriction = wheel.sidewaysFriction;
+        sidewaysFriction.extremumSlip /= brakeSkidMultiplier;
+        sidewaysFriction.extremumValue /= brakeSkidMultiplier;
+        sidewaysFriction.asymptoteSlip /= brakeSkidMultiplier;
+        sidewaysFriction.asymptoteValue /= brakeSkidMultiplier;
+        wheel.sidewaysFriction = sidewaysFriction;
     }
 
     private void UpdateAllWheelPoses()
